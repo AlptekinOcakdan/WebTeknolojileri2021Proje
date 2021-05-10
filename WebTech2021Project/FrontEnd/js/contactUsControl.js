8cc8f93f2429102721c727db7fd46c4b16020d05
Alptekin Ocakdan <alptekin.ocakdan@gmail.com>
2021-05-10 19:02:52 +0300

    Creating Contact Us page, Adding Stylesheet, CSS Adjustments, contactUs.php does not work correctly.

---
+++ WebTech2021Project/FrontEnd/js/contactUsControl.js
@@ -0,0 +1,41 @@
+const name = document.getElementById('name')
+const surname = document.getElementById('surname')
+const mail = document.getElementById('mail')
+const cellphone = document.getElementById('cellphone')
+const message = document.getElementById('message')
+const errorElement = document.getElementById('error')
+
+
+let form;
+form.addEventListener('submit',(e) => {
+    let messages = []
+    if (name.value == "" || name.value == null){
+        messages.push('name must be filled')
+    }
+    if (surname.value == "" || surname.value == null){
+        messages.push('surname must be filled')
+    }
+    if (mail.value == "" || mail.value == null){
+        messages.push('e-mail must be filled')
+    }
+    if(!(mail.includes("."))){
+    messages.push('e-mail yanlış')  
+    }
+   
+    if (message.value == "" || message.value == null){
+        messages.push('message must be written')
+    }
+
+    if (cellphone.value == "" || cellphone.value == null){
+        messages.push('phone number must be written')
+    }
+
+    
+    
+    if(messages.length > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+})
+
+
---
+++ WebTech2021Project/FrontEnd/php/contactUs.php
@@ -0,0 +1,57 @@
+<?php
+  
+  error_reporting(0);
+
+  $name = $_POST['name'];
+  $surname = $_POST['surname'];
+  $gender = $_POST['gender'];
+  $birthDate = $_POST['birthDate'];
+  $mail = $_POST['mail'];
+  $cellphone = $_POST['cellphone'];
+  $message = $_POST['message'];
+  $team = $_POST['sportTeam'];
+
+
+  
+  if (empty($_POST['device1'])==false) {
+  $device1 = $_POST['device1'];
+  }
+
+  if (empty($_POST['device2'])==false) {
+  $device2 = $_POST['device2'];
+  }
+
+  if (empty($_POST['device3'])==false) {
+  $device3 = $_POST['device3'];
+  }
+ 
+ 
+
+  echo "<br>";
+  echo "isminiz :".$name."<br>";
+  echo "soyisminiz :".$surname."<br>";
+  echo "gender :".$gender."<br>";
+  echo "birth date :".$birthDate."<br>";
+  echo "mail :".$mail."<br>";
+  echo "teamınız :".$team."<br>";
+  echo "cellphone number :".$cellphone."<br>";
+  echo "message :".$message."<br>";
+ 
+
+
+  if (empty($device1)==false) {
+  echo "the devices you use to access our site : cellphone";
+  echo "<br>";
+  }
+  if (empty($device2)==false) {
+  echo "the devices you use to access our site : tablet";
+  echo "<br>";
+  }
+if (empty($device3)==false) {
+  echo "the devices you use to access our site : PC";
+  echo "<br>";
+  }
+
+  
+    
+?>
